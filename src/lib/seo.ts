63fa9390f6e481cb386c2d761b5412a82de59b97
Prabha <prabha8859@gmail.com>
2026-09-17 15:01:08 +0530

    updated some query

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -14,7 +14,7 @@ export function createPageMetadata(
   title: string,
   description: string,
   path: string,
-  image = "/og-image.png"
+  image = "/og-image.jpg"
 ): Metadata {
   const url = `${siteUrl}${path}`;
 
